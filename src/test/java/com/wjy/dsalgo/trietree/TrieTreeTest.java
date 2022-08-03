dfcf455864f687f80474af9ff8d6dd988db1721b
handsomestWei <1293986765@qq.com>
2022-08-03 11:11:18 +0800

    [add] add trietree

---
+++ src/test/java/com/wjy/dsalgo/trietree/TrieTreeTest.java
@@ -0,0 +1,39 @@
+package com.wjy.dsalgo.trietree;
+
+import java.util.List;
+
+public class TrieTreeTest {
+
+    public static void main(String[] args) {
+
+        String[] words = {"qwe", "asd", "asxc"};
+        String pref = "as";
+        // insert and countPrefix
+        TrieTree<Character> trieTree1 = new TrieTree<>();
+        for (String word : words) {
+            trieTree1.insert(TrieTreeUtil.str2CharList(word));
+        }
+        System.out.println(trieTree1.countPrefix(TrieTreeUtil.str2CharList(pref)));
+
+        // delete and countPrefix
+        String delWord = "a";
+        trieTree1.delete(TrieTreeUtil.str2CharList(delWord));
+        System.out.println(trieTree1.countPrefix(TrieTreeUtil.str2CharList(pref)));
+
+        int n = 13;
+        int k = 7;
+        // insert and findIndex
+        TrieTree<String> trieTree2 = new TrieTree<>();
+        for (int i = 1; i <= n; i++) {
+            // insert by trie sort
+            trieTree2.insert(TrieTreeUtil.int2StrList(i));
+        }
+        List<String> rs = trieTree2.findIndexByDFS(k);
+        System.out.println(TrieTreeUtil.strList2Int(rs));
+        k = 2;
+        rs = trieTree2.findIndexByDFS(k);
+        System.out.println(TrieTreeUtil.strList2Int(rs));
+
+        System.out.println(TrieTreeUtil.getTrieOrder(13));
+    }
+}
